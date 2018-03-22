838b9cc67bcb9d61155d305b382e4996b786244b
Gleb Yakovlev <brin@ya.ru>
2018-03-22 22:30:56 +0500

    Сделал предпоследнюю форму

--- public/js/main.js
+++ public/js/main.js
@@ -2,4 +2,4 @@ $(document).ready(function () {
 	$('input, select').styler();
 });
 
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJtYWluLmpzIl0sInNvdXJjZXNDb250ZW50IjpbIiQoZG9jdW1lbnQpLnJlYWR5KGZ1bmN0aW9uICgpIHtcblx0JCgnaW5wdXQsIHNlbGVjdCcpLnN0eWxlcigpO1xufSk7XG4iXSwiZmlsZSI6Im1haW4uanMifQ==
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJtYWluLmpzIl0sInNvdXJjZXNDb250ZW50IjpbIiQoZG9jdW1lbnQpLnJlYWR5KGZ1bmN0aW9uICgpIHtcclxuXHQkKCdpbnB1dCwgc2VsZWN0Jykuc3R5bGVyKCk7XHJcbn0pO1xyXG4iXSwiZmlsZSI6Im1haW4uanMifQ==
